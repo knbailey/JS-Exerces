b25dad0ef0427e596b5bd36bccfdaee28b9477cc
knbailey <knbailey831@gmail.com>
2022-03-29 14:48:24 -0500

    Conditionals complete

---
+++ Conditionals/practice.js
@@ -0,0 +1,45 @@
+// let randomNumber = Math.round(Math.random());
+
+// if (randomNumber) {
+//   console.log('Yes!');
+// } else {
+//   console.log('No.');
+// }
+
+// let randomNumber = Math.round(Math.random());
+
+// console.log(randomNumber ? 'Yes!' : 'No.');
+
+//let weather = 'sunny'; // 'cloudy', 'rainy', 'cold', etc
+
+// if (weather === 'sunny') {
+//   console.log(`It's a beautiful day!`);
+// } else if (weather === 'rainy') {
+//   console.log(`Grab your umbrella.`);
+// } else {
+//   console.log(`Let's stay inside.`);
+// }
+
+let weather = 'cloudy'; // 'cloudy', 'rainy', 'cold', etc
+
+// switch (weather) {
+//   case 'sunny':
+//     console.log(`It's a beautiful day!`);
+//     break;
+//   case 'rainy':
+//     console.log(`Grab your umbrella.`);
+//     break;
+//   case 'hot':
+//     console.log(`Shorts and sleeves today.`);
+//     break;
+//   default:
+//     console.log(`Let's stay inside.`);
+// }
+
+// let speed = 0;
+// let acceleration = 24;
+// let brakingForce = 19;
+
+// let isMoving = (brakingForce < acceleration && speed > 0) || acceleration > 0;
+
+// console.log(isMoving);
