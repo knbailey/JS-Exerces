c955510ccbbc751b3e7a7be1cc1b40c78353d780
knbailey <knbailey831@gmail.com>
2022-03-30 21:56:35 -0500

    Variable Scope and Strings

---
+++ Strings/practice.js
@@ -0,0 +1,53 @@
+// const str1 = "These aren't the droids you're looking for.";
+
+// console.log(string.length);
+
+// const str2 = 'confetti floating everywhere';
+
+// console.log(str2.toUpperCase());
+
+// function repeat(n, string) {
+//   reps = '';
+
+//   while (n > 0) {
+//     reps += string;
+//     n -= 1;
+//   }
+//   return reps;
+// }
+
+// console.log(repeat(3, 'ha')); // 'hahaha'
+
+// let rhyme = 'A pirate I was meant to be!\nTrim the sails and roam the sea!';
+
+// console.log(rhyme);
+
+// let string1 = 'Polar Bear';
+// let string2 = 'Polar bear';
+// let string3 = 'Penguin';
+
+// console.log(string1.toLowerCase() === string2.toLowerCase());
+// console.log(string2.toLowerCase() === string3.toLowerCase());
+
+// let byteSequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu';
+
+// console.log(byteSequence.includes('x'));
+
+// function isBlank(string) {
+//   return string.trim().length === 0;
+// }
+
+// console.log(isBlank('mars')); // false
+// console.log(isBlank('  ')); // false
+// console.log(isBlank('')); // true
+
+let string = 'launch school tech & talk';
+let words = string.split(' ');
+let capWord = [];
+
+for (let i = 0; i < words.length; i++) {
+  let word = words[i];
+  capWord.push(word[0].toUpperCase() + word.slice(1));
+}
+
+console.log(capWord.join(' '));
