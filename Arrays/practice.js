5ba14cc10990d3a4a00c49fc3080408290b169ba
knbailey <knbailey831@gmail.com>
2022-03-30 23:26:33 -0500

    Arrays

---
+++ Arrays/practice.js
@@ -0,0 +1,83 @@
+// function first(array) {
+//   return array[0];
+// }
+// console.log(first(['Earth', 'Moon', 'Mars']));
+
+// function last(array) {
+//   return array[array.length - 1];
+
+// }
+// console.log(last(['Earth', 'Moon', 'Mars']));
+
+// let energy = ['fossil', 'solar', 'wind', 'tidal', 'fusion'];
+// energy.shift();
+// energy.push('geothermal');
+
+// console.log(energy);
+
+// let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+// console.log(alphabet.split(''));
+
+// let scores = [96, 47, 113, 89, 100, 102];
+// let count = 0;
+
+// for (let i = 0; i < scores.length; i++) {
+//   if (scores[i] >= 100) {
+//     count += 1;
+//   }
+// }
+// console.log(count);
+
+let vocabulary = [
+  ['happy', 'cheerful', 'merry', 'glad'],
+  ['tired', 'sleepy', 'fatigued', 'drained'],
+  ['excited', 'eager', 'enthused', 'animated'],
+];
+
+// for (let vi = 0; vi < vocabulary.length; vi++) {
+//   let words = vocabulary[vi];
+
+//   for (let wordsi = 0; wordsi < words.length; wordsi++) {
+//     console.log(words[wordsi]);
+//   }
+// }
+
+// let destinations = [
+//   'Prague',
+//   'London',
+//   'Sydney',
+//   'Belfast',
+//   'Rome',
+//   'Aruba',
+//   'Paris',
+//   'Bora Bora',
+//   'Barcelona',
+//   'Rio de Janeiro',
+//   'Marrakesh',
+//   'New York City',
+// ];
+
+// function contains(question, answer) {
+//   return answer.indexOf(question) >= 0;
+// }
+
+// console.log(contains('Barcelona', destinations)); // true
+// console.log(contains('Nashville', destinations)); // falseconsole.log(
+
+// let passcode = ['11', 'jZ5', 'hQ3f*', '8!7g3', 'p3Fs'];
+// console.log(passcode.join('-'));
+
+// let groceryList = [
+//   'paprika',
+//   'tofu',
+//   'garlic',
+//   'quinoa',
+//   'carrots',
+//   'broccoli',
+//   'hummus',
+// ];
+
+// while (groceryList.length > 0) {
+//   let check = groceryList.shift();
+//   console.log(check);
+// }
